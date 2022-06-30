656c1d6c044392e766b67bd7685ca6136933bc6b
Profsain <profsaincrown@gmail.com>
2022-06-30 18:09:07 +0100

    Add Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import "../styles/Home.css";
 
 const Home = () => {
-  const [categories, setCategories] = useState([]);
+  const [mealsCategory, setMealCategory] = useState([]);
   const url = "https://www.themealdb.com/api/json/v1/1/categories.php"
 
   useEffect(() => {
@@ -10,16 +11,30 @@ const Home = () => {
         if (response.ok) {
           return response.json();
         }
-        // throw response;
       })
       .then((data) => {
-        setCategories(data);
-      })
+        const dataArr = data.categories;
+        setMealCategory(dataArr);
+      });
   }, []);
-  console.log(categories)
+
+  const categories = mealsCategory.map((items) => {
+    return (
+      <div className="Meal-card">
+        <img src={items.strCategoryThumb} alt={items.strCategory} />
+        <h4>{items.strCategory}</h4>
+        <button type="button" className="Explore-btn">Explore Meals</button>
+      </div>
+    )
+  });
+  const totalCategories = mealsCategory.length;
+
   return (
     <div className="Home-container">
-      <h2>This is Home Page showing Meals categories</h2>
+      <h2 className="Home-title">Checkout ({totalCategories}) Meals in Categories</h2>
+      <div className="Category-grid">
+        {categories}
+      </div>
     </div>
   )
 }
