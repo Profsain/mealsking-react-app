228ebc395c41a84e30f618835ea57065b3c398a5
Profsain <profsaincrown@gmail.com>
2022-07-02 15:53:56 +0100

    Add meals category list component

--- src/components/MealExplore.js
+++ src/components/MealExplore.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Header from "./Header";
 import "../styles/MealExplore.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const MealExplore = () => {
   const location = useLocation();
@@ -28,7 +29,10 @@ const MealExplore = () => {
       <div className="Meals-item" key={items.idMeal}>
         <img src={items.strMealThumb} alt={items.strMeal} />
         <div className="Meals-headline">
-          <h4>{items.strMeal} Like - Dislike</h4>
+          <h4>{items.strMeal}</h4>
+          <FontAwesomeIcon icon="fa-light fa-heart" />
+          <FontAwesomeIcon icon="fa-solid fa-heart" />
+          <FontAwesomeIcon icon="fa-solid fa-thumbs-up" />
         </div>
         <div className="Button-container">
           <button type="button">View</button>
